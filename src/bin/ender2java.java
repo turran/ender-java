24e0a69582ca4f91a6398122bfeea4caf0092fd9
Jorge Zapata <jorgeluis.zapata@gmail.com>
2015-07-13 01:11:49 +0200

    Start generating some code

--- src/bin/ender2java.java
+++ src/bin/ender2java.java
@@ -7,51 +7,68 @@ import java.util.List;
 import java.io.File;
 import java.io.IOException;
 import com.sun.codemodel.JCodeModel;
+import com.sun.codemodel.JClass;
+import com.sun.codemodel.JDefinedClass;
+import com.sun.codemodel.JClassAlreadyExistsException;
 
 public class ender2java {
+	private static JCodeModel cm;
+	private static Lib lib;
+
+	public static void generateObject(ItemObject o)
+	{
+		List<ItemFunction> functions = o.getFunctions();
+		try {
+			JDefinedClass cls = cm._class("org." + lib.getName() + "." + o.getName());
+			ItemObject parent = o.getInherit();
+			if (parent != null)
+			{
+				JClass parentCls = cm.ref("org." + lib.getName() + "." + parent.getName());
+				cls._extends(parentCls);
+			}
+
+		} catch (JClassAlreadyExistsException ex) {
+			ex.printStackTrace();
+		}
+
+		for (int j = 0; j < functions.size(); j++)
+		{
+			ItemFunction f = functions.get(j);
+			int flags = f.getFlags();
+			//if (flags & ItemFunction.Flags.CTOR)
+			//	System.out.println("is ctor");
+			// flags.contains(ItemFunction.Flags.CTOR)
+		}
+	}
 
 	public static void main(String[] args)
 	{
 		Ender.init();
-		Lib lib = Lib.find(args[0]);
-		System.out.println("Lib " + args[0] + " version " + lib.getVersion() + " name " + lib.getName());
+
+		lib = Lib.find(args[0]);
+		if (lib == null)
+			return;
+
 		List<Lib> dependencies = lib.getDependencies();
 		for (int i = 0; i < dependencies.size(); i++)
 		{
 			System.out.println("Dep: " + dependencies.get(i).getName());
 		}
 
-		JCodeModel cm = new JCodeModel();
+		cm = new JCodeModel();
 
 		// Generate objects
 		List<Item> objects = lib.listItem(ItemType.OBJECT);
 		for (int i = 0; i < objects.size(); i++)
 		{
 			ItemObject o = (ItemObject)objects.get(i);
-			System.out.println("Object " + o.getName());
-			ItemObject parent = o.getInherit();
-			while (parent != null)
-			{
-				//System.out.println("+ " + parent.getName());
-				parent = parent.getInherit();
-			}
-
-			List<ItemFunction> functions = o.getFunctions();
-			for (int j = 0; j < functions.size(); j++)
-			{
-				ItemFunction f = functions.get(j);
-				int flags = f.getFlags();
-				System.out.println("Function " + f.getName() + " flags " + flags);
-				//if (flags & ItemFunction.Flags.CTOR)
-				//	System.out.println("is ctor");
-				// flags.contains(ItemFunction.Flags.CTOR)
-			}
+			generateObject(o);
 		}
 
 		try {
 			cm.build(new File("."));
 		} catch (IOException ex) {
-
+			ex.printStackTrace();
 		}
 		Ender.shutdown();
 	}
