82f84be6c97b1a81469f1e4a384aaaced391cc6f
Jorge Zapata <jorgeluis.zapata@gmail.com>
2015-07-14 20:24:14 +0200

    Start generating some code

--- src/bin/ender2java.java
+++ src/bin/ender2java.java
@@ -8,9 +8,12 @@ import java.io.File;
 import java.io.IOException;
 
 import com.sun.codemodel.JCodeModel;
+import com.sun.codemodel.JMod;
+import com.sun.codemodel.JMods;
 import com.sun.codemodel.JClass;
 import com.sun.codemodel.JDefinedClass;
 import com.sun.codemodel.JClassAlreadyExistsException;
+import com.sun.codemodel.ClassType;
 
 public class ender2java {
 	private static JCodeModel cm;
@@ -19,26 +22,50 @@ public class ender2java {
 	public static void generateObject(ItemObject o)
 	{
 		List<ItemFunction> functions = o.getFunctions();
+		ItemFunction ctor = null;
+		ItemFunction ref = null;
+		ItemFunction unref = null;
+
+		// Check some information before creating the class
+		for (int j = 0; j < functions.size(); j++)
+		{
+			ItemFunction f = functions.get(j);
+			int flags = f.getFlags();
+
+			if ((flags & ItemFunctionFlag.CTOR.getValue()) == ItemFunctionFlag.CTOR.getValue())
+				ctor = f;
+			if ((flags & ItemFunctionFlag.REF.getValue()) == ItemFunctionFlag.REF.getValue())
+				ref = f;
+			if ((flags & ItemFunctionFlag.UNREF.getValue()) == ItemFunctionFlag.UNREF.getValue())
+				unref = f;
+		}
+
+
 		try {
-			JDefinedClass cls = cm._class("org." + lib.getName() + "." + o.getQualifiedClassName());
+			int mods = JMod.NONE;
+
+			if (ctor == null)
+			{
+				mods |= JMod.ABSTRACT;
+			}
+
+			JDefinedClass cls = cm._class(mods, "org." + o.getQualifiedClassName(), ClassType.CLASS);
+			if (ref != null && unref != null)
+			{
+				JClass referenceableCls = cm.ref("org.ender.common.ReferenceableObject");
+				cls._implements(referenceableCls);
+			}
+
 			ItemObject parent = o.getInherit();
 			if (parent != null)
 			{
-				JClass parentCls = cm.ref("org." + lib.getName() + "." + parent.getQualifiedClassName());
+				JClass parentCls = cm.ref("org." + parent.getQualifiedClassName());
 				cls._extends(parentCls);
 			}
 		} catch (JClassAlreadyExistsException ex) {
 			ex.printStackTrace();
 		}
 
-		for (int j = 0; j < functions.size(); j++)
-		{
-			ItemFunction f = functions.get(j);
-			int flags = f.getFlags();
-			//if (flags & ItemFunction.Flags.CTOR)
-			//	System.out.println("is ctor");
-			// flags.contains(ItemFunction.Flags.CTOR)
-		}
 	}
 
 	public static void main(String[] args)
